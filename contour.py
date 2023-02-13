15fcd8fef31b5fb4a1576c5a8b3b4ca0a1d97a12
RedGlimmer <107247457+RedGlimmer@users.noreply.github.com>
2023-02-13 12:22:25 +0100

    Update contour.py

--- contour.py
+++ contour.py
@@ -7,14 +7,28 @@ def detection(tresh1, tresh2):
     # Detect edges using Canny
     canny_output = cv.Canny(src_gray, tresh1, tresh2)
     kernel = np.ones((5,5))
-    imgDil = cv.dilate(canny_output, kernel, iterations = 2)
+    imgDil = cv.dilate(canny_output, kernel, iterations = 1)
     # Find contours
-    contours, hierarchy = cv.findContours(imgDil, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv.findContours(imgDil, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     # Draw contours
     drawing = np.zeros((imgDil.shape[0], imgDil.shape[1], 3), dtype=np.uint8)
+
+    for cnt in contours:
+        area = cv.contourArea(cnt)
+        areaMin = cv.getTrackbarPos("AreaMin", "Params")
+        areaMax = cv.getTrackbarPos("AreaMax", "Params")
+        if area > areaMin and area < areaMax:
+            cv.drawContours(drawing, cnt, -1, (255, 255, 2550), 1)
+            peri = cv.arcLength(cnt, True)
+            approx = cv.approxPolyDP(cnt, 0.02 * peri, True)
+            x, y, w, h = cv.boundingRect(approx)
+            cv.rectangle(resultImg, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+
+    """
     for i in range(len(contours)):
         color = (255, 255, 255)
-        cv.drawContours(drawing, contours, i, color, 1, cv.LINE_8, hierarchy, 0)
+        cv.drawContours(drawing, contours, i, color, 1, cv.LINE_8, hierarchy, 0)        
         # Object detection
         cnt = contours[i]
         (x,y),radius = cv.minEnclosingCircle(cnt)
@@ -27,6 +41,7 @@ def detection(tresh1, tresh2):
             cv.putText(img = resultImg, text = "",#f"{center[0]}, {center[1]}",
                          org = (center[0], center[1]), fontFace = cv.FONT_HERSHEY_TRIPLEX,
                         fontScale = 1, color = (0, 255, 0), thickness = 1)
+        """
 
     # Show in a window
     cv.imshow("Source frame", frame)
@@ -38,8 +53,10 @@ def empty(a):
 
 cv.namedWindow("Params")
 cv.resizeWindow("Params", 640, 240)
-cv.createTrackbar("thresh1", "Params", 150, 255, empty)
-cv.createTrackbar("thresh2", "Params", 150, 500, empty)
+cv.createTrackbar("thresh1", "Params", 255, 255, empty)
+cv.createTrackbar("thresh2", "Params", 500, 500, empty)
+cv.createTrackbar("AreaMin", "Params", 1000, 30000, empty)
+cv.createTrackbar("AreaMax", "Params", 5000, 30000, empty)
 
 webcam = cv.VideoCapture(0)
 
@@ -49,7 +66,7 @@ while True:
     if ok == True:
         frameBlur = cv.GaussianBlur(frame, (7,7), 1)
         src_gray = cv.cvtColor(frameBlur, cv.COLOR_BGR2GRAY)
-        src_gray = cv.blur(src_gray, (3,3))
+        #src_gray = cv.blur(src_gray, (3,3))
 
         tresh1 = cv.getTrackbarPos("thresh1", "Params")
         tresh2 = cv.getTrackbarPos("thresh1", "Params")
